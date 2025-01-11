cee091dc5c230b9fd6f7fd1e16748825486c9d98
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2025-01-11 23:34:29 +0500

    Products pages

--- server/middlewares/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Middleware for handling asynchronous functions
-module.exports = function asyncHandler(handler) {
-  return function (req, res, next) {
-    Promise.resolve(handler(req, res, next)).catch(next);
-  };
-};
