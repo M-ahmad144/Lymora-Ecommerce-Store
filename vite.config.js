efad508e197008bfa13e9593260fe4b3a445e2a3
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2025-01-17 20:27:26 +0500

    Integration with the Backend

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://lymora.choreoapps.dev",
+        target:
+          "https://5d7cf89b-12ae-43b7-949c-42e2f8982e90.e1-us-east-azure.choreoapps.dev/",
+
         // target: "http://localhost:5000",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
