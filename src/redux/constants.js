9d2cb77313ec081ba7cfb57bd0d7297b58a94893
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2025-01-18 19:16:23 +0500

    deployment

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://lymora.choreoapps.dev";
+export const BASE_URL =
+  "https://5d7cf89b-12ae-43b7-949c-42e2f8982e90.e1-us-east-azure.choreoapps.dev";
 // export const BASE_URL = "http://localhost:5000";
 
 export const USER_URL = "/api/users";
