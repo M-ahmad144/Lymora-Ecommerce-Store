a7a15f73666f7041d88c707814b64cb750d739ca
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2025-01-18 14:02:31 +0500

    production...

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: true,
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://lymora.choreoapps.dev",
+        changeOrigin: true,
+        secure: true,
       },
     },
-    define: {
-      "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
-    },
-  };
+  },
 });
