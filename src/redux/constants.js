ed3feb1e8d90f28b18493f4dc0aedf7174ed5bd5
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2025-01-19 00:01:42 +0500

    backend production url

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL =
-  "https://5d7cf89b-12ae-43b7-949c-42e2f8982e90.e1-us-east-azure.choreoapps.dev";
+// export const BASE_URL =
+//   "https://5d7cf89b-12ae-43b7-949c-42e2f8982e90.e1-us-east-azure.choreoapps.dev";
 // export const BASE_URL = "http://localhost:5000";
-
+export const BASE_URL = "https://lymora.choreoapps.dev";
 export const USER_URL = "/api/users";
 export const CATEGORY_URL = "/api/categories";
 export const PRODUCT_URL = "/api/products";
