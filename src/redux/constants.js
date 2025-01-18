170b7f9f8cfd9645df006453d97cc7922436315e
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2025-01-18 14:39:17 +0500

    production3

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -5,10 +5,3 @@ export const PRODUCT_URL = "/api/products";
 export const UPLOAD_URL = "/api/uploads";
 export const ORDERS_URL = "/api/orders";
 export const PAYPAL_URL = "/api/config/paypal";
-
-// export const USER_URL = "https://lymora.choreoapps.dev/api/users";
-// export const CATEGORY_URL = "https://lymora.choreoapps.dev/api/categories";
-// export const PRODUCT_URL = "https://lymora.choreoapps.dev/api/products";
-// export const UPLOAD_URL = "https://lymora.choreoapps.dev/api/uploads";
-// export const ORDERS_URL = "https://lymora.choreoapps.dev/api/orders";
-// export const PAYPAL_URL = "https://lymora.choreoapps.dev/api/config/paypal";
